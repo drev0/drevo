4a8eeed0bce1ff6b07a9fba9f67e571c0666bfc2
Alexander Snov <snov.contact@gmail.com>
2019-09-02 18:11:14 +0700

    Add drevo core-react interaction prototype

--- packages/core/src/index.ts
+++ packages/core/src/index.ts
@@ -1,3 +1,13 @@
 const _version = require("../package").version as string;
 
 export const version = () => _version;
+
+export interface DrevoParams<N> {
+  root: N;
+}
+
+export const drevo = <N = unknown>(params: DrevoParams<N>) => {
+  return params.root;
+};
+
+export type AnyDrevo<T> = T;
--- packages/react/src/Components.tsx
+++ packages/react/src/Components.tsx
@@ -1,12 +1,41 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { version as coreVersion, drevo, AnyDrevo } from "@drevo/core";
 
 import { version } from "./version";
 
 interface DrevoProps {}
 
-export const Drevo: React.FC<DrevoProps> = ({ children }) => (
-  <div>
-    <div>Drevo {version()}</div>
-    {children}
-  </div>
-);
+type DrevoState = AnyDrevo<number>;
+
+const EXPOSE_META_TO_WINDOW = process.env.NODE_ENV !== "production";
+
+export const Drevo: React.FC<DrevoProps> = ({ children }) => {
+  const [_drevo, _setDrevo] = useState<DrevoState | null>(null);
+
+  useEffect(() => {
+    if (EXPOSE_META_TO_WINDOW && typeof window !== "undefined") {
+      (window as any).__DREVO_REACT__ = {
+        version,
+        coreVersion,
+        drevo
+      };
+    }
+  }, []);
+
+  function createDrevo() {
+    _setDrevo(
+      drevo({
+        root: 420
+      })
+    );
+  }
+
+  return (
+    <div>
+      <button onClick={createDrevo}>create drevo</button>
+      <div>Drevo React {version()}</div>
+      <div>{_drevo}</div>
+      {children}
+    </div>
+  );
+};
